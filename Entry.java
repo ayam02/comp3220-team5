ff3d6da6e10e7326338762d89fd45c6798dd281d
aya-m1 <83889617+ayam02@users.noreply.github.com>
2024-11-05 15:11:56 -0500

    removed type T from Entry and replaced it with Object

--- Entry.java
+++ Entry.java
@@ -1,12 +1,12 @@
-public class Entry<T> {
+public class Entry {
     private String type; // the type is set a string that can hold the value "STRING", "INTEGER", "FLOAT"
-    private T value; // can be of any type such as integer, float or string depending on the data set
+    private Object value; // can be of any type such as integer, float or string depending on the data set
     private String field; // the name of the column using the keys from the config file
 
     public Entry() {
     }
 
-    public Entry(String type, T value, String field) {
+    public Entry(String type, Object value, String field) {
         this.field = field;
         this.value = value;
         this.type = type;
@@ -20,7 +20,7 @@ public class Entry<T> {
         return type;
     }
 
-    public T getValue() {
+    public Object getValue() {
         return value;
     }
 
@@ -32,7 +32,7 @@ public class Entry<T> {
         this.type = type;
     }
 
-    public void setValue(T value) {
+    public void setValue(Object value) {
         this.value = value;
     }
 
