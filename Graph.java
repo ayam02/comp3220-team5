7bfe7685df500da8f49723f28abeef3327988cef
aya-m1 <83889617+ayam02@users.noreply.github.com>
2024-11-05 18:10:18 -0500

    created Graph abstract class

--- Graph.java
+++ Graph.java
@@ -1,3 +1,41 @@
-public class Graph {
+public abstract class Graph {
+    private String x;
 
+    private String y;
+    private String description;
+
+    public Graph() {
+    }
+
+    public Graph(String x, String y, String description) {
+        this.x = x;
+        this.y = y;
+        this.description = description;
+    }
+
+    public abstract void createGraph();
+
+    public void setX(String x) {
+        this.x = x;
+    }
+
+    public String getX() {
+        return x;
+    }
+
+    public void setY(String y) {
+        this.y = y;
+    }
+
+    public String getY() {
+        return y;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
