a5236742bdb76edc4e59844a1e6b4f29d5b485f8
aya-m1 <83889617+ayam02@users.noreply.github.com>
2024-11-05 18:16:30 -0500

    added line graph constructors

--- LineGraph.java
+++ LineGraph.java
@@ -1,8 +1,19 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class LineGraph extends Graph {
     private List<Float> xCoordinates;
     private List<Float> yCoordinates;
+
+    public LineGraph(List<Float> xCoordinates, List<Float> yCoordinates) {
+        this.xCoordinates = xCoordinates;
+        this.yCoordinates = yCoordinates;
+    }
+
+    public LineGraph() {
+        xCoordinates = new ArrayList<>();
+        yCoordinates = new ArrayList<>();
+    }
 
     public void setxCoordinates(List<Float> xCoordinates) {
         this.xCoordinates = xCoordinates;
