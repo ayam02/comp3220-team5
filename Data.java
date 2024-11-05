1992cf1005e16a041252e73f4ed9aad9dd88ec12
aya-m1 <83889617+ayam02@users.noreply.github.com>
2024-11-05 15:12:13 -0500

    created Data class

--- Data.java
+++ Data.java
@@ -1,3 +1,22 @@
+import java.util.List;
+
 public class Data {
+    private List<Entry> row;
+
+    public Data() {
+
+    }
+
+    public Data(List<Entry> row) {
+        this.row = row;
+    }
+
+    public List<Entry> getRow() {
+        return row;
+    }
+
+    public void setRow(List<Entry> row) {
+        this.row = row;
+    }
 
 }
