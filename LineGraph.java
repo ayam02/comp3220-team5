41a2a24f9a9d8fcf093ab566ed89021b192884d6
aya-m1 <83889617+ayam02@users.noreply.github.com>
2024-11-05 18:13:40 -0500

    implemented line graph class

--- LineGraph.java
+++ LineGraph.java
@@ -1,3 +1,26 @@
-public class LineGraph {
+import java.util.List;
 
+public class LineGraph extends Graph {
+    private List<Float> xCoordinates;
+    private List<Float> yCoordinates;
+
+    public void setxCoordinates(List<Float> xCoordinates) {
+        this.xCoordinates = xCoordinates;
+    }
+
+    public List<Float> getxCoordinates() {
+        return xCoordinates;
+    }
+
+    public void setyCoordinates(List<Float> yCoordinates) {
+        this.yCoordinates = yCoordinates;
+    }
+
+    public List<Float> getyCoordinates() {
+        return yCoordinates;
+    }
+
+    @Override
+    public void createGraph() {
+    }
 }
